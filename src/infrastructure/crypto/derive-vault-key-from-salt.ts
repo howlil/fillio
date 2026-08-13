e42aaaba7cdb780ba01add13151e07e04b8436f1
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-08-14 00:13:49 +0700

    refactor: remove superseded salt decoder

--- src/infrastructure/crypto/derive-vault-key-from-salt.ts
+++
@@ -1,9 +0,0 @@
-import { decodeVaultBytes } from './vault-bytes';
-import { deriveVaultKey } from './vault-key';
-
-export function deriveVaultKeyFromSalt(
-  phrase: string,
-  encodedSalt: string,
-): Promise<CryptoKey> {
-  return deriveVaultKey(phrase, decodeVaultBytes(encodedSalt));
-}
