c72ef60ebc36a7b004f2b7e89bcd1c5f1dbf529d
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-08-13 16:59:28 +0700

    test: expose floating panel behavioral red

---
+++ src/ui/floating/FloatingPanel.tsx
@@ -0,0 +1,10 @@
+import type { PageAnalysisSummary } from '../../application/forms/analyze-field-contexts';
+
+type FloatingPanelProps = {
+  summary: PageAnalysisSummary;
+  onFill: () => void;
+};
+
+export function FloatingPanel(_props: FloatingPanelProps) {
+  throw new Error('Not implemented');
+}
